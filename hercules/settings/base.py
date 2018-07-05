8808b096919931e49e1c0e5ee38c43b49c58d482
julianapeace <chancecordelia@gmail.com>
2018-07-05 18:30:56 -0500

    added heroku to allowed host

--- hercules/settings/base.py
+++ hercules/settings/base.py
@@ -27,7 +27,10 @@ BASE_DIR = os.path.dirname(
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', None)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'herc-django.herokuapp.com',
+    'localhost',
+]
 
 
 # Application definition
